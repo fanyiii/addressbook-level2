94653416860a928fe7e60fad81ba92a18e881bbb
Fanyi <zfy926@gmail.com>
2017-02-01 22:59:10 +0800

    remove import of IllegalValueException to Street and Unit class

--- src/seedu/addressbook/data/person/Street.java
+++ src/seedu/addressbook/data/person/Street.java
@@ -1,8 +1,5 @@
 package seedu.addressbook.data.person;
 
-import seedu.addressbook.data.exception.IllegalValueException;
-
-
 /**
  * Represents a Person's street in the address book.
  * Guarantees: immutable
--- src/seedu/addressbook/data/person/Unit.java
+++ src/seedu/addressbook/data/person/Unit.java
@@ -1,6 +1,5 @@
 package seedu.addressbook.data.person;
 
-import seedu.addressbook.data.exception.IllegalValueException;
 
 /**
  * Represents a Person's unit number in the address book.
