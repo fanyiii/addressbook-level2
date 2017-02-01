53f47d9c2f89231bf7cc04df486ffd1c052d723e
Fanyi <zfy926@gmail.com>
2017-02-01 22:42:52 +0800

    Modify isValidAddress

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -11,13 +11,13 @@ public class Address {
     public static final String EXAMPLE = "123, some street, some unit, some postal code";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = 
     		"Person addresses must consist of four strings seperated by commas";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
     public static final String FIELD_SEPARATOR = ", ";
     
     private static final int BLOCK_INDEX = 0;
     private static final int STREET_INDEX = 1;
     private static final int UNIT_INDEX = 2;
     private static final int POSTAL_CODE_INDEX = 3;
+    private static final int TOTAL_FIELDS = 4;
     
     
     private Block block;
@@ -36,21 +36,21 @@ public class Address {
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
+        addressFields = trimmedAddress.split(FIELD_SEPARATOR);
         this.isPrivate = isPrivate;
-        if (!isValidAddress(trimmedAddress)) {
+        if (!isValidAddress(addressFields)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
         this.value = trimmedAddress;
-        addressFields = trimmedAddress.split(FIELD_SEPARATOR);
+        
         setFields(addressFields);
     }
 
     /**
      * Returns true if a given string is a valid person email.
      */
-    public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
-        
+    public static boolean isValidAddress(String[] test) {
+        return test.length == TOTAL_FIELDS && PostalCode.isValidPostalCode(test[POSTAL_CODE_INDEX]);
     }
 
     private void setFields(String[] fields) {
