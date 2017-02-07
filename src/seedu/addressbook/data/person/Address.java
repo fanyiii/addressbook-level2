be6782821d2af625f570af77cca829bc6a24facf
Fanyi <zfy926@gmail.com>
2017-02-08 00:17:08 +0800

    implement getPrintableString in Address, Email, Name, Phone

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address implements Printable{
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
@@ -39,6 +39,14 @@ public class Address {
     @Override
     public String toString() {
         return value;
+    }
+    
+    /**
+     * Produce a printable string representation of Address
+     */    
+    @Override
+    public String getPrintableString() {
+    	return "Address: " + toString();
     }
 
     @Override
--- src/seedu/addressbook/data/person/Phone.java
+++ src/seedu/addressbook/data/person/Phone.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's phone number in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidPhone(String)}
  */
-public class Phone {
+public class Phone implements Printable{
 
     public static final String EXAMPLE = "123456789";
     public static final String MESSAGE_PHONE_CONSTRAINTS = "Person phone numbers should only contain numbers";
@@ -39,6 +39,14 @@ public class Phone {
     @Override
     public String toString() {
         return value;
+    }
+
+    /**
+     * Produce a printable string representation of Phone
+     */    
+    @Override
+    public String getPrintableString() {
+    	return "Phone: " + toString();
     }
 
     @Override
