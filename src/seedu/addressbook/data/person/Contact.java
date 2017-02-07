0e0bcb5624cbe36f93cdf3211edf31b1c2e1c0cd
Fanyi <zfy926@gmail.com>
2017-02-07 22:29:20 +0800

    Extract commonalities from Phone, Email and Address classes into a superclass Contact

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,37 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Contact {
+
+    public final String value;
+    private boolean isPrivate;
+  
+    /**
+     * Validates given contact.
+     *
+     * @throws IllegalValueException if given contact string is invalid.
+     */    
+    public Contact(String contact, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        this.value = contact.trim();
+    }
+    
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+    
+    public String toString() {
+        return value;
+    }
+    
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
